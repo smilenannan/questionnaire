fede58ab4f5796074b2ca225c694670aaa255026
shoho <shoho.miura@gmail.com>
2017-03-08 21:21:25 +0900

    add confirmation and ensure the state of logging in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins, only: [:sign_in, :session]
+  devise_for :admins, only: [:session, :password]
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
