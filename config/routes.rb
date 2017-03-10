480eaab493e60eed0dda08867727e8711c608dec
shoho <shoho.miura@gmail.com>
2017-03-11 00:44:23 +0900

    make question branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :admins, :controllers => {
+    :sessions => 'admins/sessions'
+  }
+
   root to: "home#index"
+
+  namespace :admins do
+    resources :home, only: [:index]
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
