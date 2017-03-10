4e2e9c5e0232d55f64295639d31d6d2a01d9b4fb
shoho <shoho.miura@gmail.com>
2017-03-10 15:27:38 +0900

    add admin_home

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -7,9 +7,9 @@ class Admins::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    redirect_to admins_home_index_path  
+  end
 
   # DELETE /resource/sign_out
   # def destroy
