92c9d671ba9e613e079b6376b9fb16339f332b18
shoho <shoho.miura@gmail.com>
2017-03-10 14:56:25 +0900

    generate admin_controllers

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,6 +1,25 @@
 class Admins::SessionsController < Devise::SessionsController
-  def create
-    p "create!!!"
-    #redirect_to admins_home_index_path
-  end
+# before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
