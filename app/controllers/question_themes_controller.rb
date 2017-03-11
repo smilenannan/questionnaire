df8d1704ce71a6275f0b9d1608d5464f0be4562f
shoho <shoho.miura@gmail.com>
2017-03-11 15:16:00 +0900

    define destroy action

--- app/controllers/question_themes_controller.rb
+++ app/controllers/question_themes_controller.rb
@@ -2,4 +2,10 @@ class QuestionThemesController < ApplicationController
   def index
     @question_themes = QuestionTheme.all  
   end
+
+  def destroy
+    @question_theme = QuestionTheme.find(params[:id])
+    @question_theme.destroy
+    redirect_to question_themes_path
+  end
 end
