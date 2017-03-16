2b9fa183753d40cbe94048e08bdb4cff2504e8eb
shoho <shoho.miura@gmail.com>
2017-03-17 01:08:36 +0900

    make new question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,16 @@
 class QuestionsController < ApplicationController
   def index
+    p 'debug!!!!!!!!!'
+    p params
     @question_theme = QuestionTheme.find(params[:question_theme_id])
+    @question = Question.new(:question_theme_id=>params[:question_theme_id])
+  end
+
+  def create
+    @question_theme = QuestionTheme.find(params[:question_theme_id])
+    @question = @question_theme.questions.build(params.require(:question).permit(:title, :category))
+    @question.save
+
+    redirect_to question_theme_questions_path(params[:question_theme_id])
   end
 end
